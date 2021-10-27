8a7c2bcf5a92fcea4dd4527b18a5cc0e2c67e4d9
ITO Eigo <itoh@ccg.ac.jp>
2021-10-27 17:48:19 +0900

    問題4の解答例

--- Main.java
+++ Main.java
@@ -4,16 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         // 変数宣言・初期化
-        long width, height;
+        double width, height;
         Scanner sc = new Scanner(System.in);
 
         // キーボードからの入力（width/横の長さ）
         System.out.print("width: ");
-        width = sc.nextLong();
+        width = sc.nextDouble();
 
         // キーボードからの入力（height/縦の長さ）
         System.out.print("height: ");
-        height = sc.nextLong();
+        height = sc.nextDouble();
 
         // 周囲の長さの計算・表示
         System.out.print("circumference: ");
